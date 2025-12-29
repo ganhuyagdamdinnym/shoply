b4fb81d7dc0abb694db65ff576536361082dc96b
Daimaa47 <damdinnymg@gmail.com>
2025-12-29 19:01:20 +0800

    sgdfjh

--- src/pages/orderEdit.tsx
+++ src/pages/orderEdit.tsx
@@ -241,7 +241,11 @@ const OrderEdit = () => {
                       />
                     </div>
                   </div>
-                  <div className="grid w-full items-center gap-1.5 max-w-none!"></div>
+                  <div className="grid w-full items-center gap-1.5 max-w-none!">
+                    <label className="flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50 mb-1">
+                      Хороо / Баг
+                    </label>
+                  </div>
                 </div>
                 {/* <div className="col-span-1 xl:col-span-4 flex flex-col gap-6">
                  
